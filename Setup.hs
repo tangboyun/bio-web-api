4bddfc1adaaf676b7d7242cde1b381a36fca7ebf
Boyun Tang <tangboyun@BoyunTang-Arch.localdomain>
2012-04-11 14:37:26 +0800

    first commit

---
+++ Setup.hs
@@ -0,0 +1,15 @@
+#! /usr/bin/env runhaskell
+-----------------------------------------------------------------------------
+-- |
+-- Module : 
+-- Copyright : (c) 2012 Boyun Tang
+-- License : BSD-style
+-- Maintainer : tangboyun@hotmail.com
+-- Stability : experimental
+-- Portability : ghc
+--
+-- 
+--
+-----------------------------------------------------------------------------
+import Distribution.Simple
+main = defaultMain
