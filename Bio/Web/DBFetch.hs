a032fd0ef25fdced2a6b569c32b4b25a50fc4d19
Boyun Tang <tangboyun@BoyunTang-Arch.localdomain>
2012-04-19 20:39:06 +0800

    remove redandunt modules

--- Bio/Web/DBFetch.hs
+++ Bio/Web/DBFetch.hs
@@ -16,24 +16,20 @@ module Bio.Web.DBFetch
        
        where
 
-import Data.ByteString (ByteString)
-import Data.ByteString.Char8 (pack,unpack)
-import Network.Shpider
-import Bio.Web.DBFetch.Types
-import Bio.Sequence.GB.Parser
-import Network.HTTP.Conduit
-import Bio.Sequence.GB.Types hiding (source)
-import Data.Conduit.Attoparsec
-import Data.Conduit
-import Data.Attoparsec.ByteString.Char8
-import Control.Applicative
-import Bio.Web.Internal.Patiently
-import Data.Maybe
-import Data.List
-import qualified Data.Map as M
+import           Bio.Sequence.GB.Parser
+import           Bio.Sequence.GB.Types            hiding (source)
+import           Bio.Web.DBFetch.Types
+import           Control.Applicative
+import           Data.Attoparsec.ByteString.Char8
+import           Data.ByteString                  (ByteString)
+import           Data.ByteString.Char8            (pack,unpack)
+import           Data.Conduit
+import           Data.Conduit.Attoparsec
+import           Data.List
+import qualified Data.Map                         as M
+import           Data.Maybe
+import           Network.HTTP.Conduit
 
-waitTime :: Int
-waitTime = 10
 
 defaultPara :: Para
 defaultPara = Para RefSeqN FORMAT_Default
@@ -42,15 +38,6 @@ parseDB :: Parser (Maybe [GBRecord])
 parseDB = Just <$> parseGBs <|> Nothing <$ string "ERROR 12 No entries found."
 
 seqFetch :: Para -> [ByteString] -> IO [Maybe SeqRecord]
--- seqFetch (Para db format) geneIds  = runShpider $ do
---   (_,p) <- patiently waitTime download url
---   let result = eitherResult $ flip feed "" $ parse parseDB $ pack $ source p
---   case result of
---     Left str -> error $ "ERROR: " ++ str
---     Right Nothing -> return $ replicate (length geneIds) Nothing
---     Right (Just seqs) -> do
---       let m = M.fromList $ zip (map (locusName . locus) seqs) seqs
---       return $ map (fmap SR . (`M.lookup` m)) geneIds    
 seqFetch (Para db format) geneIds  = do
   request <- parseUrl url
   result <- withManager $ \manager -> do
