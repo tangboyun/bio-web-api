81d07ac1017464360257a51faa609f8eedc7adef
Boyun Tang <tangboyun@BoyunTang-Arch.localdomain>
2012-04-19 20:35:51 +0800

    Network.Shpider may cause stack overflow when fetch too much seqs per round. Now DBFetch use http-conduit to implement lazy fetch.

--- Bio/Web/DBFetch.hs
+++ Bio/Web/DBFetch.hs
@@ -21,7 +21,10 @@ import Data.ByteString.Char8 (pack,unpack)
 import Network.Shpider
 import Bio.Web.DBFetch.Types
 import Bio.Sequence.GB.Parser
+import Network.HTTP.Conduit
 import Bio.Sequence.GB.Types hiding (source)
+import Data.Conduit.Attoparsec
+import Data.Conduit
 import Data.Attoparsec.ByteString.Char8
 import Control.Applicative
 import Bio.Web.Internal.Patiently
@@ -36,29 +39,31 @@ defaultPara :: Para
 defaultPara = Para RefSeqN FORMAT_Default
 
 parseDB :: Parser (Maybe [GBRecord])
-parseDB = (fmap (Just) parseGBs) <|> (string "ERROR 12 No entries found." *> return Nothing)
+parseDB = Just <$> parseGBs <|> Nothing <$ string "ERROR 12 No entries found."
 
 seqFetch :: Para -> [ByteString] -> IO [Maybe SeqRecord]
-seqFetch (Para db format) geneIds  = runShpider $ do
-  (_,p) <- patiently waitTime download url
-  let result = eitherResult $ flip feed "" $ parse parseDB $ pack $ source p
+-- seqFetch (Para db format) geneIds  = runShpider $ do
+--   (_,p) <- patiently waitTime download url
+--   let result = eitherResult $ flip feed "" $ parse parseDB $ pack $ source p
+--   case result of
+--     Left str -> error $ "ERROR: " ++ str
+--     Right Nothing -> return $ replicate (length geneIds) Nothing
+--     Right (Just seqs) -> do
+--       let m = M.fromList $ zip (map (locusName . locus) seqs) seqs
+--       return $ map (fmap SR . (`M.lookup` m)) geneIds    
+seqFetch (Para db format) geneIds  = do
+  request <- parseUrl url
+  result <- withManager $ \manager -> do
+    Response _ _ _ bsrc <- http request manager
+    bsrc $$ sinkParser parseDB
   case result of
-    Left str -> error $ "ERROR: " ++ str
-    Right Nothing -> return $ replicate (length geneIds) Nothing
-    Right (Just seqs) -> do
+    Nothing -> return $ replicate (length geneIds) Nothing
+    Just seqs -> do
       let m = M.fromList $ zip (map (locusName . locus) seqs) seqs
-      return $ map (fmap SR . (`M.lookup` m)) geneIds    
+      return $ map (fmap SR . (`M.lookup` m)) geneIds          
   where
     baseURL = "http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?"
     url = baseURL ++ "db=" ++ toPara db ++ 
           "&style=" ++ toPara Raw ++ "&id=" ++ (
           intercalate "," $ map unpack geneIds)
 
-seqFetch' (Para db format) geneIds  = runShpider $ do
-  (_,p) <- patiently waitTime download url
-  return $ parse parseGBs $ pack $ source p
-  where
-    baseURL = "http://www.ebi.ac.uk/Tools/dbfetch/dbfetch?"
-    url = baseURL ++ "db=" ++ toPara db ++ 
-          "&style=" ++ toPara Raw ++ "&id=" ++ (
-          intercalate "," $ map unpack geneIds)
